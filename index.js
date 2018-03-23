57357a0df264d80add4732a37697eee8e76f8e6e
Neo <neo.hung@quantatw.com>
2018-03-23 16:14:18 +0800

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require("http")
 var express = require("express")
 var WebSocketServer = require("ws").Server
-
+var nonce = require('nonce')();
 var app = express()
 var port = process.env.PORT || 5000
 
@@ -15,10 +15,17 @@ var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
 wss.on("connection", function(ws) {
-	
+  console.log("websocket connection open, create timer")
   var id = setInterval(function() {
 	console.log("websocket interval")
-    //ws.send(JSON.stringify(new Date()), function() {  })
+	var msg = {
+      type: "message",
+      x: Math.round((Math.random()*100))/100,
+      y: Math.round((Math.random()*100))/100,
+      id: nonce(),
+      date: Date.now()
+    };
+    ws.send(JSON.stringify(msg))
   }, 1000)
 
  
