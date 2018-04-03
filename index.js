030333a7049daf0e86f0cc7eb547284900d35b18
Neo <neo.hung@quantatw.com>
2018-04-03 16:48:04 +0800

    test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var http = require("http")
 var express = require("express")
 var WebSocketServer = require("ws").Server
 var nonce = require('nonce')();
+var Neo = require('./neo.js')
 var app = express()
 var port = process.env.PORT || 5000
 
@@ -13,19 +14,53 @@ console.log("neo http server listening on %d", port)
 
 var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
+//
+var n = new Neo("key","secret");
+console.log("New neo created")
+
+//var returnTickerCB = function(err, result) {
+//    console.log(err)
+//    console.log(result)
+//}
 
 wss.on("connection", function(ws) {
   console.log("websocket connection open, create timer")
   var id = setInterval(function() {
-	var msg = {
+	 /* 
+	let msg = {
       type: "message",
       x: Date.now(),
-      y: Math.floor(Math.random()*100+1),
+      //y: Math.floor(Math.random()*100+1),
+      y: 0,
       id: nonce(),
       date: Date.now()
     };
-	//console.log(msg.date)
-    ws.send(JSON.stringify(msg))
+	*/
+	// if (callback && typeof(callback) === 'function') {
+    //    callback(greeting, name)
+    //}
+	
+	n.returnOrderBook("USDT","BTC", function(err,result){
+		this.seq = result['seq'];
+		this.bids = result['bids']; //50 unit
+		this.asks = result['asks'];
+		this.msg = {
+          type: "message",
+          x: Date.now(),
+          y: this.bids[0][0],
+          id: nonce(),
+          date: Date.now()
+        };
+		//console.log(this.bids[0][0])
+	}.bind(this));
+	if (typeof(this.msg) != "undefined"){
+		//console.log(this.msg.y)
+		ws.send(JSON.stringify(this.msg))
+	}
+      //console.log("2:"+this.seq)
+	
+	//
+	
   }, 1000)
 
  
---
+++ neo.js
@@ -0,0 +1,136 @@
+module.exports = (function(){
+  var request = require('request')
+  var fs      = require("fs")
+  var nonce = require("nonce")()
+  var crypto  = require('crypto')
+  var version     = '0.0.8'
+  //PUBLIC_API_URL  = 'https://poloniex.com/public',
+  //PRIVATE_API_URL = 'https://poloniex.com/tradingApi',
+  var USER_AGENT    = ""
+  //var USER_AGENT    = 'poloniex.js ' + version;
+  //USER_AGENT  = 'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0'
+  var PUBLIC_API_URL  = 'https://poloniex.com/public'
+  //var PRIVATE_API_URL = 'https://www.google.com'
+  var PRIVATE_API_URL = 'https://poloniex.com/tradingApi'
+  
+  function joinCurrencies(currencyA, currencyB) {
+      if (typeof currencyB !== 'string') {
+        return currencyA;
+      }
+      return currencyA + '_' + currencyB;
+  }
+ //實作N()函數, 在module.exports最後return N()函數
+ function N(key, secret) {
+ 	console.log(key)
+ 	console.log(secret)
+	//這裡定義了計算加密特徵值的函數傳輸檔頭時使用
+ 	this._getPrivateHeaders = function(parameters) {
+ 		var paramString, signature
+ 		paramString = Object.keys(parameters).map(function(param) {
+ 			return encodeURIComponent(param) + '=' + encodeURIComponent(parameters[param]);
+ 		}).join("&")
+		//用該KEY算出加密特徵值
+ 		signature = crypto.createHmac('sha512', secret).update(paramString).digest('hex')
+ 	return {
+        Key: key,
+        Sign: signature,
+      };
+ 	}
+ }
+ //N.prototype定義需要的函數
+  N.prototype = {
+  	_request: function(options, callback) {
+  		//
+  		if (!('headers' in options)) {
+        	options.headers = {};
+    	}
+    	options.json = true;
+        options.headers['User-Agent'] = USER_AGENT
+        options.strictSSL = false //Poloniex.STRICT_SSL;
+        options.timeout = 10 * 1000;
+
+  		//執行
+  		request(options, function(err, response, body) {
+      		if (!err && (typeof body === 'undefined' || body === null)){
+      			err = 'Empty response';
+      		}
+      		callback(err, body)
+      	})
+  		return this	
+  	},
+  	//public其實是get方式呼叫
+  	_public: function(command, parameters, callback) {
+  		//console.log(command)
+  		//console.log(parameters)
+  		//console.log(callback)
+		var options;
+		//如果只有兩個參數, 參數2是callback不是parameters
+      	if (typeof parameters === 'function') {
+        	callback = parameters;
+        	parameters = {};
+      	}
+      	parameters || (parameters = {});
+        parameters.command = command;
+        options = {
+          method: 'GET',
+          url: PUBLIC_API_URL,
+          //參數1為＆後面參數,放在.command
+          qs: parameters
+        };
+        options.qs.command = command;
+      	return this._request(options, callback);
+  	},
+  	//private其實是poset方式呼叫
+  	_private: function(command, parameters, callback) {
+      var options;
+      if (typeof parameters === 'function') {
+        callback = parameters;
+        parameters = {};
+      }
+      parameters || (parameters = {});
+      parameters.command = command;
+      parameters.nonce = nonce();
+
+      options = {
+        method: 'POST',
+        url: PRIVATE_API_URL,
+        //參數1為POST參數,放在.command
+        form: parameters,
+        //處理檔頭
+        headers: this._getPrivateHeaders(parameters)
+      };
+      //console.log(options.headers)
+      return this._request(options, callback);
+    },  	
+  	returnTicker: function(callback) {
+      return this._public('returnTicker', callback);
+    },
+    return24hVolume: function(callback) {
+      return this._public('return24hVolume', callback);
+    },
+    returnCurrencies: function(callback) {
+      return this._public('returnCurrencies', callback);
+    },
+    returnLoanOrders: function(currency, callback) {
+      return this._public('returnLoanOrders', {currency: currency}, callback);
+    },
+    returnOrderBook: function(currencyA, currencyB, callback) {
+    	var currencyPair;
+      	if (typeof currencyB === 'function') {
+        	currencyPair = currencyA;
+        	callback = currencyB;
+        	currencyB = null;
+      	}
+      else {
+        currencyPair = joinCurrencies(currencyA, currencyB);
+      }
+      var parameters = {
+        currencyPair: currencyPair
+      };
+      return this._public('returnOrderBook', parameters, callback);
+    },
+
+  }
+  //N.prototype.f2 = N.prototype.f1;
+  return N
+})()
